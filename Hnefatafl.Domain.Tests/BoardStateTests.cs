0ce98a3c19b0535e86fa4cbb23a81bf813690f73
hegej <hege.jacobsen12@gmail.com>
2025-08-08 19:35:39 +0200

    feat: In doiman, sealed immutable BoardState with add/remove helpers and add basic tests

---
+++ Hnefatafl.Domain.Tests/BoardStateTests.cs
@@ -0,0 +1,17 @@
+﻿using Hnefatafl.Domain;
+using Xunit;
+
+public class BoardStateTests
+{
+
+    [Fact]
+    public void WithPiece_return_new_instance_while_keep_the_original()
+    {
+        var b1 = BoardState.Empty();
+        var b2 = b1.WithPiece(new('E', 6), new Piece(PieceType.King));
+
+        Assert.False(b1.TryGetPiece(new('E', 6), out _)); //uendret
+        Assert.True(b2.TryGetPiece(new('E', 6), out var p) && p.Type == PieceType.King);
+    }
+
+}
