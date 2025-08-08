37c8bd83f5a951c5e2bdcd0cc25a201a80e5d6bd
hegej <hege.jacobsen12@gmail.com>
2025-08-09 00:57:57 +0200

    Feat: Tests added for Move value

---
+++ Hnefatafl.Domain.Tests/MoveTests.cs
@@ -0,0 +1,13 @@
+﻿using Hnefatafl.Domain;
+using Xunit;
+
+public class MoveTests
+{
+    [Fact]
+    public void ToString_prints_nicely()
+    {
+        var m = new Move(new('E', 6), new('E', 8));
+        Assert.Equal("E6->E8", m.ToString());
+    }
+}
+
