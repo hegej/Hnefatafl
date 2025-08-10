0b35bb15c78082719bb07cc79d7bb6b13448d1d1
hegej <hege.jacobsen12@gmail.com>
2025-08-10 20:07:26 +0200

    Feat: Added RuleEngine to create rules for movement, and comments what should be added

---
+++ Hnefatafl.Domain/RuleEngine.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Hnefatafl.Domain;
+
+public sealed class RuleEngine
+{
+    /* Rules to validate the movements (no capture)
+     *  - From != To
+     *  - Positions is on the board
+     *  - Check that piece stands on the From space
+     *  - Movement is orthogonal : Same lane or rank
+     *  - Destination is empty
+     *  - Only KING can stop on throne of corners
+     *  - Free lane to move FROM to TO (can't pass other pieces, and only pass throne if empty)
+    */
+}
